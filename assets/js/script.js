4e52d403951cdbfa770f4de34f52dcf3f0b8b1a6
kellydhunter1 <kellydhunter1@gmail.com>
2021-07-05 03:57:50 -0400

    refactored element in index and script for better functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,93 @@
 const quizContainer = document.querySelector("#quiz-container");
-const startBtn = document.querySelector("#start-button");
+const quizIntroEl = document.querySelector("#quiz-intro");
+const quizQuestionsEl = document.querySelector("#quiz-questions");
+const highScoreEl = document.querySelector("#high-score");
+const startBtn = document.querySelector("#start-btn");
 const timerEl = document.querySelector("#timer");
 const quizScore = 0;
-const quizQuestions = [
+const quizQuestionsArr = [
     // copy this object format for each question, a: is number of correct answer
-    { q: '', 1: '', 2: '', 3: '',  4: '', a: 2 }
+    { q: 'This is question 1', 1: '', 2: '', 3: '',  4: '', a: 2 },
+    { q: 'This is question 2', 1: '', 2: '', 3: '',  4: '', a: 2 },
+    { q: 'This is question 3', 1: '', 2: '', 3: '',  4: '', a: 2 },
+    { q: 'This is question 4', 1: '', 2: '', 3: '',  4: '', a: 2 },
+    { q: 'This is question 5', 1: '', 2: '', 3: '',  4: '', a: 2 }
     ];
 
 /*
    QUIZ INTRO FUNCTION
   creates div for directions and start button
 */
-const quizDirectionsEl = document.createElement("div");
-const quizFormEl = document.createElement("form");
 
 const quizIntro = function () {
+// if buttons are on the page, skip create div, form, and button
   // creates div and adds directions
-
-  quizDirectionsEl.className="directions";
-  quizDirectionsEl.textContent= "Answer as many questions as you can in 1 minute. If you get a question wrong, it subtracts 5 seconds from your timer!";
-  
-  // creates form and gives class
-
-    quizFormEl.className = "quiz-form";
+  let quizDirections = document.createElement("h2");  
+    quizDirections.className="directions";
+    quizDirections.textContent= "Answer as many questions as you can in 1 minute. If you get a question wrong, it subtracts 5 seconds from your timer!";
  
-  // creates button
+    // creates button
   let startQuizBtn = document.createElement("button");
+      startQuizBtn.className = "start-btn";
+      startQuizBtn.setAttribute('id', 'start-btn');
       // adds button text
       startQuizBtn.textContent = "START QUIZ";
-    startQuizBtn.className = "btn start-btn";
 
-
-
-    // add button to form
-    quizFormEl.appendChild(startQuizBtn);
-  
     // add directions to div
-    quizContainer.appendChild(quizDirectionsEl);
+    quizIntroEl.appendChild(quizDirections);
+    // add button to div
+    quizIntroEl.appendChild(startQuizBtn);
+  };
 
-    // add form to div
-    quizContainer.appendChild(quizFormEl);
-};
 
 /*
 // STARTS THE QUIZ ON BUTTON CLICK
 */
 const quizButtonHandler = function (event) {
-  event.preventDefault();
+  var targetEl = event.target;
+
+if (targetEl.matches("#start-btn")) {
   startQuiz();
+  console.log("Start Quiz", targetEl);
+} if (targetEl.matches("#replay-btn")) {
+    quizIntroEl.textContent = "";
+    highScoreEl.textContent = "";
+    quizIntro();
+  console.log("Try Again", targetEl);
+} if (targetEl.matches("#quiz-question")){
+  console.log("next question");
+}
+else return;
 };
+
+
+const quizOver = function() {
+  // create new high score div
+
+  let highScoreText = document.createElement("h2");
+    highScoreText.textContent = "I WONDER WHAT THE HIGH SCORE WILL BE?!";
+
+   // creates play again button
+  let playAgainBtn = document.createElement("button");
+    // adds button text
+    playAgainBtn.textContent = "Try Again";
+    playAgainBtn.setAttribute('id', 'replay-btn');
+
+  // add button to div
+highScoreEl.appendChild(highScoreText);
+highScoreEl.appendChild(playAgainBtn);
+// remove correct answers div
+// quizContainer.removeChild();
+quizQuestionsEl.textContent = "";
+};
+
+
 
 /*
 // TIMER THAT COUNTS DOWN FROM 60 
 */
 function countdown() {
-  let quizTimer = 60;
+  let quizTimer = 5;
   // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function() {
     // As long as the `quizTimer` is greater than 1
@@ -73,51 +106,60 @@ function countdown() {
       // Use `clearInterval()` to stop the timer
       clearInterval(timeInterval);
       // Call the `gameOver()` function
-      gameOver();
+      quizOver();
     }
   }, 1000);
 };
 
-const quizOver = function() {
 
-};
 
 /*
 // STARTS QUIZ
 // timer starts, questions populate, keeps track of correct answers
 */
-const startQuiz = function() {
+const startQuiz = function(event) {
   // clear out quiz intro from the div
-let quizQuestionsEl = document.createElement("div");
-quizQuestionsEl.className = "quiz-questions";
-quizQuestionsEl.textContent = "JAVASCRIPT CODE QUESTIONS WILL BE HERE!";
+  quizIntroEl.textContent = "";
+  // start timer
+  countdown();
 
-quizContainer.appendChild(quizQuestionsEl);
-quizContainer.removeChild(quizFormEl);
-quizContainer.replaceChild(quizQuestionsEl, quizDirectionsEl);
 
-countdown();
+
+  // loop through questions
+  for (let i = 0; i < quizQuestionsArr.length;) {
+    quizQuestionsEl.innerHTML = "<h3 id='quiz-question'>" + quizQuestionsArr[i].q + "</h3>"
+  }; if (event.target.matches(quizQuestionsEl)) {
+    i++;
+    return;
+  };
+  
+
+
+
+
+
+
+
+  
+
+// quizQuestionsEl.appendChild(quizQuestions);
+
 };
 
 
-
-
-
 quizIntro();
+
+
 /*
-
 Event Listeners
-
 */
+
 // event listener on start button to call start quiz
-quizFormEl.addEventListener("click", quizButtonHandler);
-
-// event listener that response to answer click to bring the next question 
-
-
-
-
-
+quizIntroEl.addEventListener("click", quizButtonHandler);
+// event listener on answer selection to bring the next question 
+quizQuestionsEl.addEventListener("click", quizButtonHandler);
+// event listener on try again to start quiz intro
+highScoreEl.addEventListener("click", quizButtonHandler);
 
 
 
